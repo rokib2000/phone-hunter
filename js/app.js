d2b7656e92d4ee59a3f6302c7ecefe0d4d079016
rokib2000 <rokib0w1@gmail.com>
2022-09-01 03:56:56 +0600

    Load api And display all phone

---
+++ js/app.js
@@ -0,0 +1,38 @@
+const loadPhone = async () => {
+  const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayPhone(data.data);
+};
+
+const displayPhone = (phones) => {
+  const phoneContainer = document.getElementById("phone-container");
+  phones.forEach((phone) => {
+    // console.log(phone);
+    const phoneDiv = document.createElement("div");
+    phoneDiv.classList.add("col");
+    phoneDiv.innerHTML = `
+    <div class="card h-100">
+    <img src="${phone.image}" class="img-thumbnail p-4"  alt="..." />
+    <div class="card-body">
+      <h5 class="card-title text-center">${phone.phone_name}</h5>
+      <p class="card-text text-center"><span class="fw-semibold">Brand: </span>${phone.brand}</p>
+      <div class="d-grid gap-2 col-6 mx-auto">
+        <button
+        onclick = "phoneDetails('${phone.slug}')"
+          class="btn btn-primary"
+          type="button"
+          data-bs-toggle="modal"
+          data-bs-target="#phonedetailsModal"
+        >
+          Show details
+        </button>
+      </div>
+    </div>
+  </div>
+      `;
+    phoneContainer.appendChild(phoneDiv);
+  });
+};
+
+loadPhone();
