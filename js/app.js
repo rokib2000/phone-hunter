d85ddb85830c75206769166292119dda30980dc9
rokib2000 <rokib0w1@gmail.com>
2022-09-01 14:14:09 +0600

    added phone details show

--- js/app.js
+++ js/app.js
@@ -42,7 +42,7 @@ const displayPhone = (phones, dataLimit) => {
       <p class="card-text text-center"><span class="fw-semibold">Brand: </span>${phone.brand}</p>
       <div class="d-grid gap-2 col-6 mx-auto">
         <button
-        onclick = "phoneDetails('${phone.slug}')"
+        onclick = "phoneDetailsLoad('${phone.slug}')"
           class="btn btn-primary"
           type="button"
           data-bs-toggle="modal"
@@ -92,4 +92,82 @@ const toggleSpinner = (isLoading) => {
   }
 };
 
-// loadPhone("oppo");
+// phoneDetailsLoad
+const phoneDetailsLoad = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  phoneDetailsShow(data.data);
+};
+
+// Phone Details Show
+
+const phoneDetailsShow = (phone) => {
+  // console.log(phone);
+  const phoneTittle = document.getElementById("phonedetailsModalLabel");
+  phoneTittle.innerText = phone.name;
+
+  const phoneImageShow = document.getElementById("phone-image-show");
+  phoneImageShow.innerHTML = `<img src="${phone.image}" class="img-fluid my-3" alt="..." />`;
+
+  const phoneDetailsTable = document.getElementById("phone-details-table");
+  phoneDetailsTable.innerHTML = `
+  <table class="table table-striped">
+    <tbody>
+      <tr>
+        <th scope="row">Release Date</th>
+        <td>${phone.releaseDate}</td>
+      </tr>
+      <tr>
+        <th scope="row">Brand</th>
+        <td>${phone.brand}</td>
+      </tr>
+      <tr>
+        <th scope="row">Main Features</th>
+        <td></td>
+      </tr>
+      <tr>
+        <th scope="row">Storage</th>
+        <td>${phone.mainFeatures.storage}</td>
+      </tr>
+      <tr>
+        <th scope="row">Display size</th>
+        <td>${phone.mainFeatures.displaySize}</td>
+      </tr>
+      <tr>
+        <th scope="row">Chip set</th>
+        <td>${phone.mainFeatures.chipSet}</td>
+      </tr>
+      <tr>
+        <th scope="row">Others</th>
+        <td></td>
+      </tr>
+      <tr>
+        <th scope="row">Bluetooth</th>
+        <td>${phone.others.Bluetooth}</td>
+      </tr>
+      <tr>
+        <th scope="row">GPS</th>
+        <td>${phone.others.GPS}</td>
+      </tr>
+      <tr>
+        <th scope="row">NFC</th>
+        <td>${phone.others.NFC}</td>
+      </tr>
+      <tr>
+        <th scope="row">Radio</th>
+        <td>${phone.others.Radio}</td>
+      </tr>
+      <tr>
+        <th scope="row">USB</th>
+        <td>${phone.others.USB}</td>
+      </tr>
+      <tr>
+        <th scope="row">WLAN</th>
+        <td>${phone.others.WLAN}</td>
+      </tr>
+    </tbody>
+  </table>`;
+};
+
+loadPhone("a");
